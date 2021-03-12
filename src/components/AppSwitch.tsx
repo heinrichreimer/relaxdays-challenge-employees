9db1753b9f65f6ff151d0aafbb5d77586a9ae06e
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-13 00:38:57 +0100

    Route pages

--- src/components/AppSwitch.tsx
+++ src/components/AppSwitch.tsx
@@ -1,11 +1,46 @@
 import { FunctionComponent } from "react";
 import { Route, Switch } from "react-router";
+import {
+  Colleagues,
+  Corona,
+  Doctor,
+  Events,
+  Home,
+  NotFound,
+  Settings,
+  SickLeaves,
+  Vacation,
+} from "./pages";
 
 export const AppSwitch: FunctionComponent = () => {
   return (
     <Switch>
       <Route exact path="/">
-        Home
+        <Home />
+      </Route>
+      <Route exact path="/social">
+        <Colleagues />
+      </Route>
+      <Route exact path="/events">
+        <Events />
+      </Route>
+      <Route exact path="/vacation">
+        <Vacation />
+      </Route>
+      <Route exact path="/health/leaves">
+        <SickLeaves />
+      </Route>
+      <Route exact path="/health/doctor">
+        <Doctor />
+      </Route>
+      <Route exact path="/health/corona">
+        <Corona />
+      </Route>
+      <Route exact path="/user/settings">
+        <Settings />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   );
